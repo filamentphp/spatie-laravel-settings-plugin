bc54b3878f153fd8deeb6722cae18ffa7b0c254e
Dan <git@danharrin.com>
2023-08-21 09:36:42 +0000

    Merge branch 3.x of https://github.com/filamentphp/filament into 3.x

--- resources/lang/fr/pages/settings-page.php
+++ resources/lang/fr/pages/settings-page.php
@@ -7,7 +7,7 @@ return [
         'actions' => [
 
             'save' => [
-                'label' => 'Tallenna',
+                'label' => 'Sauvegarder',
             ],
 
         ],
@@ -17,7 +17,7 @@ return [
     'notifications' => [
 
         'saved' => [
-            'title' => 'Tallennettu',
+            'title' => 'Sauvegardé',
         ],
 
     ],
