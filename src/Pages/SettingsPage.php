80c9e960b30890fdc731da262b71255cb39bee31
Dan <git@danharrin.com>
2025-04-23 06:39:48 +0000

    Merge pull request #16096 from yandearta/3.x

--- src/Pages/SettingsPage.php
+++ src/Pages/SettingsPage.php
@@ -4,28 +4,27 @@ namespace Filament\Pages;
 
 use Filament\Actions\Action;
 use Filament\Actions\ActionGroup;
+use Filament\Forms\ComponentContainer;
+use Filament\Forms\Form;
 use Filament\Notifications\Notification;
 use Filament\Pages\Concerns\CanUseDatabaseTransactions;
 use Filament\Pages\Concerns\HasUnsavedDataChangesAlert;
-use Filament\Schemas\Components\Actions;
-use Filament\Schemas\Components\Component;
-use Filament\Schemas\Components\EmbeddedSchema;
-use Filament\Schemas\Components\Form;
-use Filament\Schemas\Components\Group;
-use Filament\Schemas\Schema;
 use Filament\Support\Exceptions\Halt;
 use Filament\Support\Facades\FilamentView;
 use Throwable;
 
 /**
- * @property-read Schema $form
+ * @property ComponentContainer $form
  */
 class SettingsPage extends Page
 {
     use CanUseDatabaseTransactions;
+    use Concerns\InteractsWithFormActions;
     use HasUnsavedDataChangesAlert;
 
     protected static string $settings;
+
+    protected static string $view = 'filament-spatie-laravel-settings-plugin::pages.settings-page';
 
     /**
      * @var array<string, mixed> | null
@@ -61,10 +60,6 @@ class SettingsPage extends Page
 
     public function save(): void
     {
-        if (! $this->canEdit()) {
-            return;
-        }
-
         try {
             $this->beginDatabaseTransaction();
 
@@ -162,14 +157,10 @@ class SettingsPage extends Page
 
     public function getSaveFormAction(): Action
     {
-        $hasFormWrapper = $this->hasFormWrapper();
-
         return Action::make('save')
             ->label(__('filament-spatie-laravel-settings-plugin::pages/settings-page.form.actions.save.label'))
-            ->submit($hasFormWrapper ? $this->getSubmitFormLivewireMethodName() : null)
-            ->action($hasFormWrapper ? null : $this->getSubmitFormLivewireMethodName())
-            ->keyBindings(['mod+s'])
-            ->visible($this->canEdit());
+            ->submit('save')
+            ->keyBindings(['mod+s']);
     }
 
     public function getSubmitFormAction(): Action
@@ -177,80 +168,29 @@ class SettingsPage extends Page
         return $this->getSaveFormAction();
     }
 
-    public function defaultForm(Schema $schema): Schema
+    public function form(Form $form): Form
     {
-        return $schema
-            ->columns(2)
-            ->disabled(! $this->canEdit())
-            ->inlineLabel($this->hasInlineLabels())
-            ->statePath('data');
+        return $form;
     }
 
-    public function form(Schema $schema): Schema
+    /**
+     * @return array<int | string, string | Form>
+     */
+    protected function getForms(): array
     {
-        return $schema;
-    }
-
-    public function content(Schema $schema): Schema
-    {
-        return $schema
-            ->components([
-                $this->getFormContentComponent(),
-            ]);
-    }
-
-    public function getFormContentComponent(): Component
-    {
-        if (! $this->hasFormWrapper()) {
-            return Group::make([
-                EmbeddedSchema::make('form'),
-                $this->getFormActionsContentComponent(),
-            ]);
-        }
-
-        return Form::make([EmbeddedSchema::make('form')])
-            ->id('form')
-            ->livewireSubmitHandler($this->getSubmitFormLivewireMethodName())
-            ->footer([
-                $this->getFormActionsContentComponent(),
-            ]);
-    }
-
-    public function getFormActionsContentComponent(): Component
-    {
-        return Actions::make($this->getFormActions())
-            ->alignment($this->getFormActionsAlignment())
-            ->fullWidth($this->hasFullWidthFormActions())
-            ->sticky($this->areFormActionsSticky());
-    }
-
-    protected function getSubmitFormLivewireMethodName(): string
-    {
-        return 'save';
-    }
-
-    public function hasFormWrapper(): bool
-    {
-        return true;
-    }
-
-    protected function hasFullWidthFormActions(): bool
-    {
-        return false;
+        return [
+            'form' => $this->form(
+                $this->makeForm()
+                    ->schema($this->getFormSchema())
+                    ->statePath('data')
+                    ->columns(2)
+                    ->inlineLabel($this->hasInlineLabels()),
+            ),
+        ];
     }
 
     public function getRedirectUrl(): ?string
     {
         return null;
-    }
-
-    public function canEdit(): bool
-    {
-        return true;
-    }
-
-    public function getDefaultTestingSchemaName(): ?string
-    {
-        return 'form';
     }
 }
--- src/SpatieLaravelSettingsPluginServiceProvider.php
+++ src/SpatieLaravelSettingsPluginServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace Filament;
 
+use Illuminate\Filesystem\Filesystem;
 use Illuminate\Support\ServiceProvider;
 
 class SpatieLaravelSettingsPluginServiceProvider extends ServiceProvider
@@ -9,11 +10,47 @@ class SpatieLaravelSettingsPluginServiceProvider extends ServiceProvider
     public function boot(): void
     {
         if ($this->app->runningInConsole()) {
-            $this->commands([
-                Commands\MakeSettingsPageCommand::class,
-            ]);
+            $this->commands($this->getCommands());
+
+            $this->publishes([
+                __DIR__ . '/../resources/views' => resource_path('views/vendor/filament-spatie-laravel-settings-plugin'),
+            ], 'filament-spatie-laravel-settings-plugin-views');
+
+            foreach (app(Filesystem::class)->files(__DIR__ . '/../stubs/') as $file) {
+                $this->publishes([
+                    $file->getRealPath() => base_path("stubs/filament/{$file->getFilename()}"),
+                ], 'filament-stubs');
+            }
         }
 
         $this->loadTranslationsFrom(__DIR__ . '/../resources/lang', 'filament-spatie-laravel-settings-plugin');
+        $this->loadViewsFrom(__DIR__ . '/../resources/views', 'filament-spatie-laravel-settings-plugin');
+    }
+
+    /**
+     * @return array<class-string>
+     */
+    protected function getCommands(): array
+    {
+        $commands = [
+            Commands\MakeSettingsPageCommand::class,
+        ];
+
+        $aliases = [];
+
+        foreach ($commands as $command) {
+            $class = 'Filament\\Commands\\Aliases\\' . class_basename($command);
+
+            if (! class_exists($class)) {
+                continue;
+            }
+
+            $aliases[] = $class;
+        }
+
+        return [
+            ...$commands,
+            ...$aliases,
+        ];
     }
 }
